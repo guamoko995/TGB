5956c2087a45c6cb2a837f66f607b34f49fc3b3f
guamoko995 <guamoko95@gmail.com>
2022-03-13 19:46:48 +0300

    added ignoring requests during the creation of the game world and splitting the handler

--- main.go
+++ main.go
@@ -11,19 +11,18 @@ import (
 	tgbotapi "github.com/Syfaro/telegram-bot-api"
 )
 
-// Каждому пользователю, написавшему боту соответствует один игровой
-// мир.
+// Каждому пользователю соответствует один игровой мир.
 var worlds = make(map[int64]*engine.World)
 
+// Объект API Telegram.
 var bot *tgbotapi.BotAPI
 
 func main() {
-	var err error
-
 	// Получает token из переменной окружения
 	token := os.Getenv("TGBtoken")
 
 	// Инициализирует бота.
+	var err error
 	bot, err = tgbotapi.NewBotAPI(token)
 	if err != nil {
 		panic(err)
@@ -49,62 +48,87 @@ func handler(update tgbotapi.Update) {
 		return
 	}
 
-	UserName := update.Message.Chat.UserName
 	ID := update.Message.Chat.ID
+	UserName := update.Message.Chat.UserName
 	Request := update.Message.Text
+
+	// Обработка паники, что бы фатальный запрос не положил бота.
+	defer panicHandler(ID, UserName, Request)
 
 	// Проверяет что от пользователья пришло именно текстовое сообщение.
 	if !(reflect.TypeOf(Request).Kind() == reflect.String && Request != "") {
 		return
 	}
 
-	// Обработка паники, что бы фатальный запрос не положил бота.
-	panicHandler := func() {
-		if err := recover(); err != nil {
-			fmt.Printf("fatal error:\n	user:	 %s\n	request: %s\n	error:	 %s\n", UserName, Request, fmt.Sprint(err))
-			resp := "Возникла критическая ошибка, которая вероятно скоро будет исправлена."
-			if Request == "Panic" {
-				resp = "Не паникуйте!"
-			}
-			msg := tgbotapi.NewMessage(ID, resp)
-			bot.Send(msg)
-		}
-	}
-	defer panicHandler()
-
 	// Выводит имя пользователя и сообщение в консоль.
 	fmt.Printf("%v: %v\n", UserName, Request)
 
-	// Тест отработки запроса, вызывающего панику.
+	// Преднамеренная паника для теста обработки паники.
 	if Request == "Panic" {
 		panic("Игрок паникует!")
 	}
 
-	// Если игрок пишет в первый раз после запуска бота.
-	if _, ok := worlds[ID]; !ok || Request == "/start" {
-		W := engine.World{}
-		W.Mu.Lock()
-		worlds[ID] = &W
+	// Прерывает обработку, если мир в статусе создания.
+	_, ok := worlds[ID]
+	if ok && worlds[ID] == nil {
+		return
+	}
+
+	var W *engine.World
+
+	// Запрос на создание мира (команда /start или вынужденный запрос - отсутствие в worlds ключа ID)
+	if !ok || Request == "/start" {
+
+		// Создает в карте миров ключ с nil значением для того чтобы новые запросы игнорировались до завершения создания (стр.71).
+		worlds[ID] = nil
+
+		// Информирует пользователя о потере прогресса в случае вынужденного запроса.
 		if Request != "/start" {
 			resp := "К сожалению ваш прогрес был утерян в связи с перезапуском игрового сервера. Игра начнётся заново :("
 			msg := tgbotapi.NewMessage(ID, resp)
 			bot.Send(msg)
 		}
 
+		// Информирует пользователя о скором начале игры.
 		resp := "Игра скоро начнётся..."
 		msg := tgbotapi.NewMessage(ID, resp)
 		bot.Send(msg)
 
-		W = *world.Constructor()
+		// Создает новый мир.
+		W = world.Constructor()
+		worlds[ID] = W
 	}
 
+	// Вызов внутриигрового обработчика команд.
+	inGameHandler(ID, Request)
+}
+
+// Обработчик паникующих запросов пользователя.
+func panicHandler(ID int64, UserName string, Request string) {
+	if err := recover(); err != nil {
+		// Выводит в консоль информацию о паникующем запросе.
+		fmt.Printf("fatal error:\n      user:    %s\n   request: %s\n   error:   %s\n", UserName, Request, fmt.Sprint(err))
+
+		// Сообщение пользователю.
+		resp := "Возникла критическая ошибка, которая вероятно скоро будет исправлена."
+
+		// Другое сообщение в случае преднамеренной паники пользователя.
+		if Request == "Panic" {
+			resp = "Не паникуйте!"
+		}
+		msg := tgbotapi.NewMessage(ID, resp)
+		bot.Send(msg)
+	}
+}
+
+// Внутриигровой обработчик пользовательских запросов.
+func inGameHandler(ID int64, Request string) {
 	// Блокировка доступа к игровому миру для других горутин.
 	worlds[ID].Mu.Lock()
 
 	for {
 		// Получение ответа от главного исполнителя игрового мира.
 		resp, remainder := worlds[ID].ActiveHandler.Handle(Request)
-
 		// Отправка ответа пользователю.
 		msg := tgbotapi.NewMessage(ID, resp.Msg)
 		bot.Send(msg) // Отправка основного сообщения.
@@ -116,7 +140,7 @@ func handler(update tgbotapi.Update) {
 			// Если обработан весь запрос, цикл завершается.
 			break
 		} else {
-			// Если нет, обрабатывается оставшаяся часть.
+			// Если нет, обрабатывается оставшаяся часть запроса.
 			Request = remainder
 		}
 	}
