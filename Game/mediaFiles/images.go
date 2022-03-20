fe1d5e372e826c85b4096f15b74fe52f06e5b443
guamoko995 <guamoko95@gmail.com>
2022-03-20 17:55:51 +0300

    add finish text & image

--- Game/mediaFiles/images.go
+++ Game/mediaFiles/images.go
@@ -1,5 +1,6 @@
 package mediafiles
 
 var Image = map[string]string{
-	"Juno": "AgACAgIAAxkBAAKmHGIw4QTuMmdy6uM8h4mBbD1yKl_MAALmvDEbQBxpSQPdw7rtLCQwAQADAgADeQADIwQ",
+	"Juno finish": "AgACAgIAAxkBAAKmHGIw4QTuMmdy6uM8h4mBbD1yKl_MAALmvDEbQBxpSQPdw7rtLCQwAQADAgADeQADIwQ",
+	"Juno start":  "AgACAgIAAxkBAAKsAAFiNykNfhlzgMPs297xn0GY6H4Z5AACnrsxG2uIuEmZJrsmk-MSZwEAAwIAA3MAAyME",
 }
